0e7c345069e126db36447dbd14a2e6c7819f520e
MK3064 <maryamkarim30@outlook.com>
2026-09-02 15:14:25 +0100

    Customer DTO, mapper and service

--- src/main/java/com/sparta/movieproject/mappers/CustomerMapper.java
+++ src/main/java/com/sparta/movieproject/mappers/CustomerMapper.java
@@ -1,4 +1,13 @@
 package com.sparta.movieproject.mappers;
 
+import com.sparta.movieproject.dto.CustomerDto;
+import com.sparta.movieproject.entities.Customer;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface CustomerMapper {
+
+    CustomerDto toDto(Customer customer);
+
+    Customer toEntity(CustomerDto customerDto);
 }
