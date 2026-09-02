9f715ec8f463dd6cbbc9b628812fd8df4bc61307
albertinaripanu01 <aripanu@spartaglobal.com>
2026-09-02 15:11:44 +0100

    movie repo, mapper, dto

--- src/main/java/com/sparta/movieproject/dto/MovieDto.java
+++ src/main/java/com/sparta/movieproject/dto/MovieDto.java
@@ -1,4 +1,13 @@
 package com.sparta.movieproject.dto;
 
 public class MovieDto {
+    private int id;
+    private String title;
+
+    //constructor
+    public MovieDto() {}
+
+    public MovieDto(int id, String title) {}
+
+
 }
--- src/main/java/com/sparta/movieproject/mappers/MovieMapper.java
+++ src/main/java/com/sparta/movieproject/mappers/MovieMapper.java
@@ -1,4 +1,11 @@
 package com.sparta.movieproject.mappers;
 
+import com.sparta.movieproject.dto.MovieDto;
+import com.sparta.movieproject.entities.Movie;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface MovieMapper {
+    MovieDto toDTO(Movie movie);
+    Movie toEntity(MovieDto movieDto);
 }
